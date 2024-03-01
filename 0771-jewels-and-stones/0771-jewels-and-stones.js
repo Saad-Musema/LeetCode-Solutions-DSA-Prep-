7f3ca88f75de302d7d5d77716a89a2086bf21f3c
Saad-Musema <55858480+Saad-Musema@users.noreply.github.com>
2024-03-01 19:20:32 +0300

    Time: 59 ms (34.75%), Space: 49.8 MB (24.31%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+    let jewelSet = new Set();
+    
+    for(let i=0; i<jewels.length; i++){
+        jewelSet.add(jewels[i]);
+    }
+    
+    let count = 0;
+    
+    for(let i = 0; i< stones.length; i++){
+        if(jewelSet.has(stones[i])){
+            count++;
+        }
+    }
+    
+    return count;
+};
